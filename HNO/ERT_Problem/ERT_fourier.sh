21d1ec783755f5f79010c016ec52c20331c43682
sinapordanesh <sina.pordanesh@yahoo.com>
2024-11-25 13:37:26 -0500

    Restore ERT_Problem folder after force pull

--- HNO/ERT_Problem/ERT_fourier.sh
+++ HNO/ERT_Problem/ERT_fourier.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=r_fourier_2d_ert_s2_2
+#SBATCH --job-name=r_fno_ert_pq
 #SBATCH --output=%x_%j.out 
-#SBATCH --time=6:00:00               
+#SBATCH --time=4:00:00               
 #SBATCH --ntasks=1                    
 #SBATCH --cpus-per-task=8            
 #SBATCH --mem=64G                     
@@ -12,7 +12,8 @@ echo "Starting run at: `date`"
 # ---------------------------------------------------------------------
 
 source $HOME/repos/XNO/.venv/bin/activate
-python ./ERT_fourier.py
+
+python ERT_fourier.py --fn fno_pq --train data/s3/trainingPQ.mat --test data/s3/testPQ.mat --eval data/s3/evalPQ.mat --ts 66000
 
 # ---------------------------------------------------------------------
 echo "Job finished with exit code $? at: `date`"
--- HNO/ERT_Problem/ERT_hilbert.sh
+++ HNO/ERT_Problem/ERT_hilbert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=hilbert_2d_ert_s2_2
+#SBATCH --job-name=r_hno0_ert_k
 #SBATCH --output=%x_%j.out 
-#SBATCH --time=6:00:00               
+#SBATCH --time=4:00:00               
 #SBATCH --ntasks=1                    
 #SBATCH --cpus-per-task=8            
 #SBATCH --mem=64G                     
@@ -13,12 +13,12 @@ echo "Starting run at: `date`"
 
 source $HOME/repos/XNO/.venv/bin/activate
 
-python ./ERT_hilbert.py
+python ERT_hilbert.py --fn hno0_k --train data/s1/trainingK.mat --test data/s1/testK.mat --eval data/s1/evalK.mat --ts 80000
 
 # ---------------------------------------------------------------------
 echo "Job finished with exit code $? at: `date`"
 # ---------------------------------------------------------------------
 
 
-# squeue -j 36882790
+# squeue -j 37231976
 
